7cf8a3ca6e9732c074308d9289f42d6e33728f57
Emil Hertz <emil.hertz@gmail.com>
2023-05-09 10:55:36 +0200

    Add folder and division example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # db_halvfabrikata
+
+Alt halvfabrikate er sorteret i de forskellige tematiske mapper (Subqueries, division queries, etc.)
---
+++ Subqueries_and_division/division_query.sql
@@ -0,0 +1,15 @@
+-- Below is an example of a  division query 
+-- Of all the movies produced in 2002, there are 282 that have entries registered in involved for all roles defined in the roles relation. 
+-- How many movies produced in 2011 have entries registered in involved for all roles defined in the roles relation?
+select count(*)
+from (
+        SELECT movie.title
+        FROM movie
+            JOIN involved ON involved.movieid = movie.id
+        WHERE movie.year = 2011
+        GROUP by movie.title
+        HAVING count(distinct involved."role") = (
+                select count(DISTINCT involved."role")
+                from involved
+            )
+    ) t
